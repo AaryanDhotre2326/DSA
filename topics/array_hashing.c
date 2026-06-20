7f366545b4ce03992553d5bdb230d420af5d14a9
AaryanDhotre2326 <dhotreaaryan2006@gmail.com>
2026-06-20 20:54:37 +0530

    day5: practiced array hashing

---
+++ topics/array_hashing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main() {
+    int n;
+    printf("Size: ");
+    scanf(" %d", &n);
+    int a[n];
+    for (int i=0; i<n; i++){
+        scanf(" %d", &a[i]);
+    }
+    
+    int hash[15] = {0};
+    for (int i=0; i<n; i++){
+        hash[a[i]]++;
+    }
+
+    int q;
+    printf("find: ");
+    scanf(" %d", &q);
+    while(q--){
+        int m;
+        printf("number: ");
+        scanf(" %d", &m);
+        printf("%d\n", hash[m]);
+    }
+    
+}
